7e0aad738cfdd4df9473ac8d6cef8446467a186b
SallyNasr <sally.nasr153@gmail.com>
2023-07-14 18:18:06 +0300

    start my project

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+-convert letters to number
+-add button for the semester and the course
+-delete lal course w semester
+-braek line between semesters
+-submit buttton lal gpa
+
+
+-validation lal input
+    -check if the user added number inside the credits: 0--4
+    -make sure everything is filled olut
+    -cleen input text
+    -repetition lal name
+    -clean input text
+
+
+*/ 
+
